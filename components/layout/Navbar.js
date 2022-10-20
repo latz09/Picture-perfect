9a3f1dc19a2e1cecf71e850b8c45cfdf67787799
latz09 <85132328+latz09@users.noreply.github.com>
2022-10-20 16:19:38 -0500

    opacity only on drop

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -11,16 +11,16 @@ const Navbar = () => {
 	};
 
 	const dropDownVariant = {
-		hidden: { x: '-100vw', opacity: 0 },
+		hidden: { opacity: 0 },
 		visible: {
-			x: 0,
+			// y: 0,
 			opacity: 1,
 			transition: {
 				duration: 1,
 				
 			},
 		},
-		exit: { y: '-100vh', transition: {duration: 1} },
+		exit: {opacity: 0, transition: {duration: .3} },
 	};
 
 	return (
