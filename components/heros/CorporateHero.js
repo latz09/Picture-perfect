d408ef1578ad9b6026c1ebb7ebf1028966093e5c
latz09 <85132328+latz09@users.noreply.github.com>
2022-10-24 12:59:39 -0500

    corporatehero animation

--- components/heros/CorporateHero.js
+++ components/heros/CorporateHero.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import hero from '../../public/images/corporate/office.jpg';
+import WhileInView from '../utils/animations/WhileInView';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const CorporateHero = () => {
 	return (
@@ -15,19 +17,35 @@ const CorporateHero = () => {
 				/>
 			</div>
 			<div className='relative flex justify-center items-center h-full bg-black bg-opacity-60'>
-				<div className='grid gap-4 text-center tracking-widest text-gray-200'>
-					<h1 className='text-3xl md:text-6xl font-robotoslab text-white'>
-						Commercial Services
-					</h1>
-					<span className=' text-lg md:text-2xl text-gray-300 '>
-						elegantly framed artwork your clients can enjoy
-					</span>
-					<Link href={'/commercial-services'}>
-						<div>
-							<button className='bg-red-500 p-4 hover:bg-opacity-75 transition duration-700 text-xl'>Learn More</button>
-						</div>
-					</Link>
-				</div>
+				<AnimatePresence>
+					<div className='grid gap-4 text-center tracking-widest text-gray-200'>
+						<motion.h1
+							initial={{ opacity: 0 }}
+							whileInView={{ opacity: 1 }}
+							transition={{ duration: 1.3, delay: 0.3 }}
+							exit={{ opacity: 0 }}
+							className='text-3xl md:text-6xl font-robotoslab text-white'
+						>
+							Commercial Services
+						</motion.h1>
+						<motion.p
+							initial={{ opacity: 0 }}
+							whileInView={{ opacity: 1 }}
+							transition={{ duration: 1.5, delay: .7 }}
+							exit={{ opacity: 0 }}
+							className=' text-lg md:text-2xl text-gray-300 '
+						>
+							elegantly framed artwork your clients can enjoy
+						</motion.p>
+						<Link href={'/commercial-services'}>
+							<div>
+								<button className='bg-red-500 p-4 hover:bg-opacity-75 transition duration-700 text-xl'>
+									Learn More
+								</button>
+							</div>
+						</Link>
+					</div>
+				</AnimatePresence>
 			</div>
 		</div>
 	);
