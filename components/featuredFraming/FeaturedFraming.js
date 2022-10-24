1dd5c755202939abfb089830e433b1598da74fac
latz09 <85132328+latz09@users.noreply.github.com>
2022-10-24 15:39:58 -0500

    featured framing margin x

--- components/featuredFraming/FeaturedFraming.js
+++ components/featuredFraming/FeaturedFraming.js
@@ -5,7 +5,7 @@ const FeaturedFraming = () => {
 	return (
 		<div className='grid grid-cols-2 lg:grid-cols-4 gap-5 items-center justify-center my-12 max-w-7xl mx-auto'>
 			{featuredFraming.map((item) => (
-				<span key={item.title}>
+				<span key={item.title} className="px-2">
 					<FeaturedItem image={item.image} title={item.title} />
 				</span>
 			))}
