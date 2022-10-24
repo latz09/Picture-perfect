7b8ff6d0b4bdc685d462cfa138adf40212649ef1
latz09 <85132328+latz09@users.noreply.github.com>
2022-10-24 15:03:42 -0500

    preservation animations

--- components/heros/PreservationHero.js
+++ components/heros/PreservationHero.js
@@ -1,9 +1,15 @@
 import Image from 'next/image';
 import hero from '../../public/images/preservation/step-4.jpg';
+import { motion } from 'framer-motion';
 
 const Preservationhero = () => {
 	return (
-		<div className='relative w-[100vw] h-[60vh]'>
+		<motion.div
+			className='relative w-[100vw]'
+			initial={{ height: '90vh' }}
+			animate={{ height: '40vh' }}
+			transition={{ duration: 2, delay: 1.1 }}
+		>
 			<div className='-z-10'>
 				<Image
 					src={hero}
@@ -15,17 +21,21 @@ const Preservationhero = () => {
 			</div>
 			<div className='relative flex justify-center items-center h-full bg-black bg-opacity-70 px-3'>
 				<div className='grid gap-4 text-left tracking-widest text-gray-200 items-center justify-items-center'>
-					<h1 className='text-5xl font-rubik text-white text-center'>
+					<motion.h1
+						className='text-5xl font-rubik text-white text-center'
+						initial={{ fontSize: '4.5rem', lineHeight: '1' }}
+						animate={{ fontSize: '3rem', lineHeight: '1' }}
+						transition={{ duration: 1.8, delay: 1.3 }}
+					>
 						Preservation process
-					</h1>
-					<div className='mx-12'>
+					</motion.h1>
+					<div className='max-w-3xl mx-auto text-center text-xl'>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Molestie
-						nunc non blandit massa.
+						eiusmod tempor incididunt.
 					</div>
 				</div>
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
