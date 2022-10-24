3d8d8cba45ac6626f36ff7ecfdf019e55609f838
latz09 <85132328+latz09@users.noreply.github.com>
2022-10-24 13:57:21 -0500

    wholesale animations

--- components/corporate/Blurb.js
+++ components/corporate/Blurb.js
@@ -1,21 +1,25 @@
 import { MdOutlineCorporateFare } from 'react-icons/md';
+import WhileInView from '../utils/animations/WhileInView';
 
 const Blurb = () => {
 	return (
-		<div className="max-w-3xl mx-auto  ">
-			<div className='py-12 grid gap-2 md:grid-cols-4 items-center mx-2'>
-				<div className='text-5xl text-red-500'>
-					<MdOutlineCorporateFare />
+		<WhileInView>
+			<div className='max-w-3xl mx-auto  '>
+				<div className='py-12 grid gap-2 md:grid-cols-4 items-center mx-2'>
+					<div className='text-5xl text-red-500'>
+						<MdOutlineCorporateFare />
+					</div>
+
+					<span className='col-span-3 tracking-wide md:tracking-wider leading-7 text-justify text-gray-600'>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. A
+						iaculis at erat pellentesque adipiscing commodo. Aenean euismod
+						elementum nisi quis eleifend quam adipiscing vitae. Est placerat in
+						egestas erat imperdiet sed.
+					</span>
 				</div>
-				<p className='col-span-3 tracking-wide md:tracking-wider leading-7 text-justify text-gray-600'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. A iaculis
-					at erat pellentesque adipiscing commodo. Aenean euismod elementum nisi
-					quis eleifend quam adipiscing vitae. Est placerat in egestas erat
-					imperdiet sed.
-				</p>
 			</div>
-		</div>
+		</WhileInView>
 	);
 };
 
--- components/corporate/WholeSalePricing.js
+++ components/corporate/WholeSalePricing.js
@@ -1,51 +1,62 @@
 import Link from 'next/link';
+import WhileInView from '../utils/animations/WhileInView';
 
 const WholeSalePricing = () => {
 	return (
 		<div className='pb-4 md:py-12'>
 			<div className='grid gap-3 md:grid-cols-3 items-center text-gray-500 mx-2'>
 				<div className='col-span-2 grid gap-5 order-2 md:order-1'>
-					<div className='grid gap-3 bg-gray-100 p-3 shadow-md'>
-						<h1 className='text-2xl md:text-3xl text-gray-600'>Whole Sale Pricing </h1>
-						<p className='text-justify'>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. A
-							iaculis at erat pellentesque adipiscing commodo. Aenean euismod
-							elementum nisi quis eleifend quam adipiscing vitae. Est placerat
-							in egestas erat imperdiet sed.
-						</p>
-					</div>
-					<div className='grid gap-3 bg-gray-100 p-3 shadow-md'>
-						<h1 className='text-2xl md:text-3xl text-gray-600'>
-							Delivery and installation{' '}
-						</h1>
-						<p className='text-justify'>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. A
-							iaculis at erat pellentesque adipiscing commodo. Aenean euismod
-							elementum nisi quis eleifend quam adipiscing vitae. Est placerat
-							in egestas erat imperdiet sed.
-						</p>
-					</div>
-					<div className='grid gap-3 bg-gray-100 p-3 shadow-md'>
-						<h1 className='text-2xl md:text-3xl text-gray-600'>Custom Framing</h1>
-						<p className='text-justify'>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. A
-							iaculis at erat pellentesque adipiscing commodo. Aenean euismod
-							elementum nisi quis eleifend quam adipiscing vitae. Est placerat
-							in egestas erat imperdiet sed.
-						</p>
-						<Link href={'/custom-framing'}>
-							<div className='mt-2'>
-								<button className='bg-red-500 p-4 hover:bg-opacity-75 transition duration-700 text-lg text-white'>
-									Custom Framing
-								</button>
-							</div>
-						</Link>
-					</div>
+					<WhileInView>
+						<div className='grid gap-3 bg-gray-100 p-3 shadow-md'>
+							<h1 className='text-2xl md:text-3xl text-gray-600'>
+								Whole Sale Pricing{' '}
+							</h1>
+							<p className='text-justify'>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. A
+								iaculis at erat pellentesque adipiscing commodo. Aenean euismod
+								elementum nisi quis eleifend quam adipiscing vitae. Est placerat
+								in egestas erat imperdiet sed.
+							</p>
+						</div>
+					</WhileInView>
+					<WhileInView>
+						<div className='grid gap-3 bg-gray-100 p-3 shadow-md'>
+							<h1 className='text-2xl md:text-3xl text-gray-600'>
+								Delivery and installation{' '}
+							</h1>
+							<p className='text-justify'>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. A
+								iaculis at erat pellentesque adipiscing commodo. Aenean euismod
+								elementum nisi quis eleifend quam adipiscing vitae. Est placerat
+								in egestas erat imperdiet sed.
+							</p>
+						</div>
+					</WhileInView>
+					<WhileInView>
+						<div className='grid gap-3 bg-gray-100 p-3 shadow-md'>
+							<h1 className='text-2xl md:text-3xl text-gray-600'>
+								Custom Framing
+							</h1>
+							<p className='text-justify'>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. A
+								iaculis at erat pellentesque adipiscing commodo. Aenean euismod
+								elementum nisi quis eleifend quam adipiscing vitae. Est placerat
+								in egestas erat imperdiet sed.
+							</p>
+							<Link href={'/custom-framing'}>
+								<div className='mt-2'>
+									<button className='bg-red-500 p-4 hover:bg-opacity-75 transition duration-700 text-lg text-white'>
+										Custom Framing
+									</button>
+								</div>
+							</Link>
+						</div>
+					</WhileInView>
 				</div>
-				<div className="order-1 md:order-2 mx-3 md:justify-self-center">
+				<div className='order-1 md:order-2 mx-3 md:justify-self-center'>
 					<h1 className='text-3xl md:text-4xl text-red-500 tracking-widest'>
 						Services
 					</h1>
