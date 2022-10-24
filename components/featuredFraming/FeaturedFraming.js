69dd4efa22e76dafe260b2b68d31c91a916be8fb
latz09 <85132328+latz09@users.noreply.github.com>
2022-10-24 15:48:53 -0500

    featured animations

--- components/featuredFraming/FeaturedFraming.js
+++ components/featuredFraming/FeaturedFraming.js
@@ -3,9 +3,9 @@ import { featuredFraming } from '../../data/featuredFraming';
 
 const FeaturedFraming = () => {
 	return (
-		<div className='grid grid-cols-2 lg:grid-cols-4 gap-5 items-center justify-center my-12 max-w-7xl mx-auto'>
+		<div className='grid grid-cols-2 lg:grid-cols-4 gap-2 md:gap-5 items-center justify-center my-12 max-w-7xl mx-auto px-2 '>
 			{featuredFraming.map((item) => (
-				<span key={item.title} className="px-2">
+				<span key={item.title} className="">
 					<FeaturedItem image={item.image} title={item.title} />
 				</span>
 			))}
--- components/featuredFraming/FeaturedItem.js
+++ components/featuredFraming/FeaturedItem.js
@@ -1,15 +1,21 @@
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const FeaturedItem = ({ image, title }) => {
 	return (
 		<div>
-			<div className='relative text-center grid'>
+			<div className='relative text-center grid shadow-lg '>
 				<Image src={image} alt={title} height={340} width={300} />
 
-				<div className='absolute bottom-0  bg-black bg-opacity-60 px-1 md:px-2 md:py-3'>
-					<h1 className=' text-lg md:text-xl  tracking-wide text-white'>
+				<div className='absolute bottom-0  bg-black bg-opacity-40 px-1 md:px-2 md:py-3 w-full'>
+					<motion.h1
+						className=' text-lg md:text-xl font-bold   tracking-widest text-white'
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1 }}
+						transition={{ delay: 0.4, duration: 1.5 }}
+					>
 						{title}
-					</h1>
+					</motion.h1>
 				</div>
 			</div>
 		</div>
@@ -17,5 +23,3 @@ const FeaturedItem = ({ image, title }) => {
 };
 
 export default FeaturedItem;
-
-
