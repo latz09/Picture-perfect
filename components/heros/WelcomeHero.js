02f71749452630cc4fcc8a29092134f867295e35
latz09 <85132328+latz09@users.noreply.github.com>
2022-10-24 10:54:45 -0500

    hero animation and banner

--- components/heros/WelcomeHero.js
+++ components/heros/WelcomeHero.js
@@ -1,7 +1,19 @@
 import Image from 'next/image';
 import hero from '../../public/images/front-of-building.jpg';
+import { motion } from 'framer-motion';
 
 const WelcomeHero = () => {
+	const delayVariant = {
+		hidden: { opacity: 0 },
+		visible: {
+			opacity: 1,
+			transition: {
+				delay: .5,
+				duration: 1.9,
+			},
+		},
+	};
+
 	return (
 		<div className='relative w-[100vw] h-[75vh] z-10'>
 			<Image
@@ -9,14 +21,20 @@ const WelcomeHero = () => {
 				alt=''
 				layout='fill'
 				objectFit='cover'
-				objectPosition='center'			/>
+				objectPosition='center'
+			/>
 
-			<div className='relative flex justify-center items-center h-full bg-black bg-opacity-60'>
-				<div className='grid gap-4 text-center tracking-widest text-gray-200'>
-					<h1 className='text-3xl md:text-6xl font-robotoslab text-white'>{`Picture Perfect Art & Frame`}</h1>
-					<span className=' text-lg md:text-2xl '>
+			<div className='relative flex justify-center items-center h-full bg-black bg-opacity-60 text-white'>
+				<div className='grid gap-4  tracking-widest '>
+					<h1 className='text-4xl md:text-6xl font-robotoslab font-bold px-4 md:px-0'>{`Picture Perfect Art & Frame`}</h1>
+					<motion.p
+						variants={delayVariant}
+						initial='hidden'
+						animate='visible'
+						className=' text-lg md:text-2xl  text-center  font-rubik bg-red-500 py-3 bg-opacity-75'
+					>
 						Designing the perfect framing to suit your style and budget
-					</span>
+					</motion.p>
 				</div>
 			</div>
 		</div>
