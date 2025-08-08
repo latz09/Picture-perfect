e235e681ce088a90c5c5eb0ce4065015b982b155
latz09 <85132328+latz09@users.noreply.github.com>
2025-08-08 15:20:54 -0500

    d

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -28,11 +28,11 @@ const Footer = () => {
 								Commercial Services
 							</span>
 						</Link>
-						<Link href={'/recent-work'}>
+						{/* <Link href={'/recent-work'}>
 							<span className='cursor-pointer hover:text-gray-800'>
 								Recent Work
 							</span>
-						</Link>
+						</Link> */}
 					</div>
 					<div className='justify-self-center mt-8 font-questrial font-normal'>
 						<ContactInfo />
